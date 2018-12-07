5ce1b030b11f68bd148a1f7c66288236e0633a2f
sree-86 <sriram3rhyme@gmail.com>
2018-12-06 19:30:36 -0500

    Initial Commit

---
+++ iOSAssignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAssignment2
+//
+//  Created by Sreeram Ramakrishnan on 2018-12-06.
+//  Copyright © 2018 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
